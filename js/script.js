48289c7eef5b3e4755ef2f7460bb8037bc0704e0
SaeedKabootari <saeedkabootari@gmail.com>
2022-01-03 09:59:16 -0800

    hello test

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const threedotsButton = document.querySelector('#threedots-button');
+const chevronleftButton = document.querySelector('#chevron-left-button');
+const followButton = document.querySelector('#follow-button');
+const messageButton = document.querySelector('#message-button');
+const angledownButton = document.querySelector('#angle-down-button');
+
+threedotsButton.addEventListener('click' , () => console.log('threedotsButton clicked'));
+chevronleftButton.addEventListener('click' , () => console.log('chevronleftButton clicked'));
+followButton.addEventListener('click' , () => console.log('followButton clicked'));
+messageButton.addEventListener('click' , () => console.log('messageButton clicked'));
+angledownButton.addEventListener('click' , () => console.log('angledownButton clicked'));
